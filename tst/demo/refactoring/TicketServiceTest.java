ee901a6a4061881d7291a461caf8f5ffcf75048d
Ronald Harmsen <ronald@nforza.nl>
2014-09-23 10:37:35 +0200

    Added mocking with EasyMock

--- tst/demo/refactoring/TicketServiceTest.java
+++ tst/demo/refactoring/TicketServiceTest.java
@@ -1,20 +1,8 @@
 package demo.refactoring;
 
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.closeTo;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.equalToIgnoringCase;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.lessThan;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.sameInstance;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
+import static org.easymock.EasyMock.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,33 +10,49 @@ import org.junit.Test;
 public class TicketServiceTest {
 	private TicketService service;
 	private User currentUser;
+	private TicketRepository mock;
 	
 	@Before
-	public void setUp() {
-		service = new TicketService(new FakeRepo());
+	public void setUp() throws NoTicketsAvailableException {
+		mock = createNiceMock(TicketRepository.class);
+		
+		service = new TicketService(mock);
 		currentUser = User.signIn("user", "secret");
 	}
 
 	@Test
 	public void WhenOrderingAnAvailableTicket_ItShould_ReturnTheTicket() throws NoTicketsAvailableException {
+		expect(mock.getAvailableTicketsForConcert("AAA")).andReturn(1);
+		replay(mock);
+		
 		Ticket expected = service.orderTicket("AAA", currentUser);
 		assertThat(expected, is(notNullValue()));
 	}
 
 	@Test
 	public void WhenOrderingAnAvailableTicket_ItShould_ReturnATicketForTheRightConcert() throws NoTicketsAvailableException {
+		expect(mock.getAvailableTicketsForConcert("AAA")).andReturn(1);
+		replay(mock);
+		
 		Ticket expected = service.orderTicket("AAA", currentUser);
 		assertThat(expected.getConcertCode(), is(equalTo("AAA")));
 	}
 
 	@Test(expected=NoTicketsAvailableException.class)
 	public void WhenOrderingAnUnAvailableTicket_ItShould_Throw() throws NoTicketsAvailableException {
+		expect(mock.getAvailableTicketsForConcert("DDD")).andReturn(0);
+		replay(mock);
+		
 		Ticket expected = service.orderTicket("DDD", currentUser);
 		assertThat(expected.getConcertCode(), is(equalTo("DDD")));
 	}
 	
 	@Test(expected=NoTicketsAvailableException.class)
 	public void WhenOrderingATicketAfterStockRanOut_ItShould_Throw() throws NoTicketsAvailableException {
+		expect(mock.getAvailableTicketsForConcert("AAA")).andReturn(1);
+		expect(mock.getAvailableTicketsForConcert("AAA")).andReturn(0);
+		replay(mock);
+		
 		Ticket expected = service.orderTicket("AAA", currentUser);
 		expected = service.orderTicket("AAA", currentUser);
 		assertThat(expected.getConcertCode(), is(equalTo("AAA")));
